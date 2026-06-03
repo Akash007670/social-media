535c502d661027699658a5fa66c66519b9ac70df
akash007670 <akaysharma894@gmail.com>
2026-06-03 16:49:23 +0530

    Implemented post get, update and delete controller

--- post-service/src/controllers/post-controller.js
+++ post-service/src/controllers/post-controller.js
@@ -3,10 +3,11 @@ import { validatePostCreation } from "../utils/validation.js";
 import Post from "../models/Post.js";
 
 const POST_CACHE_PREFIX = "posts";
-const POST_CACHE_TTL_SECONDS = 180; // in seconds (3 minutes)
+const POST_CACHE_TTL_SECONDS = 180; // time to live --> in seconds (3 minutes)
 
 const getPostCacheKey = (page, limit) =>
   `${POST_CACHE_PREFIX}:${page}:${limit}`;
+const getPostByIdCacheKey = (postId) => `${POST_CACHE_PREFIX}:${postId}`;
 
 const invalidatePostCache = async (req) => {
   if (!req.redisClient) return;
@@ -62,6 +63,7 @@ const createPost = async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
+
 const getAllPost = async (req, res) => {
   logger.info("Fetching all posts");
   try {
@@ -116,4 +118,104 @@ const getAllPost = async (req, res) => {
   }
 };
 
-export { createPost, getAllPost };
+const getPostById = async (req, res) => {
+  logger.info("Fetching post by ID");
+  try {
+    const postId = req.params.id;
+
+    const cacheKey = getPostByIdCacheKey(postId);
+    const cachedPost = await req.redisClient.get(cacheKey);
+
+    if (cachedPost) {
+      return res.json({
+        success: true,
+        message: "Retrieved Cached Post",
+        post: JSON.parse(cachedPost),
+      });
+    }
+
+    const post = await Post.findById(postId);
+
+    if (!post) {
+      logger.warn(`Post with ID ${postId} not found`);
+      return res
+        .status(404)
+        .json({ success: false, message: "Post not found" });
+    }
+
+    await req.redisClient.setex(
+      cacheKey,
+      POST_CACHE_TTL_SECONDS,
+      JSON.stringify(post),
+    );
+
+    return res.status(200).json({ success: true, post });
+  } catch (error) {
+    logger.error(`Error Fetching post : ${error.message}`);
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+const updatePost = async (req, res) => {
+  logger.info("Updating a post");
+
+  try {
+    const postId = req.params.id;
+    const newContent = req.body;
+
+    const updatedPost = await Post.findByIdAndUpdate(postId, newContent, {
+      new: true,
+    });
+
+    if (!updatedPost) {
+      logger.warn(`Post with ID ${postId} not found`);
+      return res
+        .status(404)
+        .json({ success: false, message: "Post not found" });
+    }
+
+    // Invalidate caches
+    await invalidatePostCache(req); // invalidate list caches (posts:*)
+    await req.redisClient.del(getPostByIdCacheKey(postId)); // invalidate detail cache (posts:${postId})
+
+    return res.status(200).json({ success: true, post: updatedPost });
+  } catch (error) {
+    logger.error(`Error Updating post : ${error.message}`);
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+const deletePost = async (req, res) => {
+  logger.info("Deleting a post");
+
+  try {
+    const postId = req.params.id;
+
+    const response = await Post.findByIdAndDelete(postId);
+
+    if (!response) {
+      logger.warn(`Post with ID ${postId} not found`);
+      return res
+        .status(404)
+        .json({ success: false, message: "Post not found" });
+    }
+
+    await invalidatePostCache(req);
+    await req.redisClient.del(getPostByIdCacheKey(postId));
+
+    return res
+      .status(200)
+      .json({ success: true, message: "Post deleted successfully" });
+  } catch (error) {
+    logger.error(`Error Deleting post : ${error.message}`);
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+export { createPost, getAllPost, getPostById, updatePost, deletePost };
--- post-service/src/routes/post-route.js
+++ post-service/src/routes/post-route.js
@@ -1,15 +1,25 @@
 import express from "express";
-
 import { authMiddleware } from "../middlewares/authMiddleware.js";
+import {
+  createPost,
+  deletePost,
+  getAllPost,
+  getPostById,
+  updatePost,
+} from "../controllers/post-controller.js";
 
 const router = express.Router();
-
-import { createPost, getAllPost } from "../controllers/post-controller.js";
 
 router.use(authMiddleware);
 
 router.post("/create", createPost);
 
 router.get("/all", getAllPost);
+
+router.get("/:id", getPostById);
+
+router.put("/:id", updatePost);
+
+router.delete("/:id", deletePost);
 
 export default router;
